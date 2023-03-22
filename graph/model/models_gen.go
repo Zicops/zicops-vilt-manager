d5b0331c70c71df184921d76b118a16bfae96c55
Ansh5461 <anshjoshi0607@gmail.com>
2023-03-23 00:11:47 +0530

    Vilt queries

--- graph/model/models_gen.go
+++ graph/model/models_gen.go
@@ -18,3 +18,31 @@ type User struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
 }
+
+type Vilt struct {
+	LspID           *string   `json:"lsp_id"`
+	CourseID        *string   `json:"course_id"`
+	NoOfLearners    *int      `json:"no_of_learners"`
+	Trainers        []*string `json:"trainers"`
+	Moderators      []*string `json:"moderators"`
+	CourseStartDate *string   `json:"course_start_date"`
+	CourseEndDate   *string   `json:"course_end_date"`
+	Curriculum      *string   `json:"curriculum"`
+	CreatedAt       *string   `json:"created_at"`
+	CreatedBy       *string   `json:"created_by"`
+	UpdatedAt       *string   `json:"updated_at"`
+	UpdatedBy       *string   `json:"updated_by"`
+	Status          *string   `json:"status"`
+}
+
+type ViltInput struct {
+	LspID           *string   `json:"lsp_id"`
+	CourseID        *string   `json:"course_id"`
+	NoOfLearners    *int      `json:"no_of_learners"`
+	Trainers        []*string `json:"trainers"`
+	Moderators      []*string `json:"moderators"`
+	CourseStartDate *string   `json:"course_start_date"`
+	CourseEndDate   *string   `json:"course_end_date"`
+	Curriculum      *string   `json:"curriculum"`
+	Status          *string   `json:"status"`
+}
